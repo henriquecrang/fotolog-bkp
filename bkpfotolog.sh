a3943e3eb10cecef9f448b206cad6a99f2cfff92
Henrique de Andrade <henriquedeandrade@gmail.com>
2016-02-04 17:22:43 -0200

    Create bkpfotolog.sh

---
+++ bkpfotolog.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#passar nome de usuario
+echo -n "Backup para fotolog.com! Entre com o nome de usuário: "
+read user
+
+mkdir $user
+cd $user
+
+# baixar pagina
+wget www.fotolog.com/$user
+
+#pegar o nome da primeira página (pode dar pau em users com numero no nome. melhorar a regex)
+page=`grep -Po '(?<=content=")[^"]*\/"' $user | grep -Po '([0-9]+)'`
+
+#criar index redirecionando para a primeira página
+echo '<META http-equiv="refresh" content="1;URL=' $page '"> ' > index.html
+
+mv $user $page
+
+mkdir files$page
+
+#encontrar lista de imagens (jpg e png)
+grep -Po '(?<=src=")[^"]*(png|jpg)' $page > files$page/temp
+#baixar imagens (jpg e png)
+cd files$page
+wget -i temp
+rm temp
+
+#mudar url das imagens para local
+
+
+#encontrar endereço da proxima pagina
+
+#mudar url de todas paginas /$user para local
+
+#remover todos os outros links que nao funcionariam
+
+
+#abrir loop para fazer isso até a última página
+
+
+#bonus: baixar mosaico e suas subpaginas
